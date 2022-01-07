1e497e121f2d4e4344f8c3a98b6586de303b1ce6
GDDMN <apawkin@gmail.com>
2022-01-07 13:04:34 +0300

    add ui labels for resources

---
+++ Assets/Scripts/UI/UIResourceLabel.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PurpleDrank
+{
+    public class UIResourceLabel : MonoBehaviour
+    {
+        ResourceManager _resManager;
+
+        public Text resourceCount;
+        public Image resourceImage;
+
+        public ResourceType resourceType;
+
+        private void Awake()
+        {
+            _resManager = FindObjectOfType<ResourceManager>();
+            foreach(var res in _resManager.resources)
+            {
+                if(res.Key == resourceType)
+                {
+                    resourceCount.text = res.Value.current.ToString();
+                    resourceImage.sprite = res.Value.icon;
+                    break;
+                }
+            }
+        }
+    }
+}
+
